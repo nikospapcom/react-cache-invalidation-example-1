7819df244080c334b439fda834aac25768339978
Nikolaos Papageorgiou <nikos19pap@gmail.com>
2020-10-03 15:04:30 +0300

    test change notification

--- src/index.js
+++ src/index.js
@@ -14,4 +14,23 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({
+  onUpdate: registration => {
+    const waitingServiceWorker = registration.waiting
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener("statechange", event => {
+        if (event.target.state === "activated") {
+          if (
+            window.confirm(
+              "There is a new version of the app ready. Please reload to update."
+            )
+          ) {
+            window.location.reload()
+          }
+        }
+      })
+      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" })
+    }
+  },
+})
